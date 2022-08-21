f5e8d5589b41b02b0accb1f299a0c93c5d4717af
GuilleFerres <guillermoferressanchez@gmail.com>
2022-08-21 12:36:43 +0200

    Daybook Layout created

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>Daybook Layout</h1>
+    <Navbar-day />
+
+    <div class="d-flex">
+        <div class="col-4">
+            <Entry-list />
+        </div>
+        <div class="col">
+            <router-view></router-view>
+        </div>
+    </div>
 </template>
+<script>
+import { defineAsyncComponent } from "vue";
+
+export default {
+    components: {
+        NavbarDay: defineAsyncComponent(() => import('../components/NavbarDay.vue')),
+        EntryList: defineAsyncComponent(() => import('../components/EntryList.vue'))
+    }
+}
+</script>
